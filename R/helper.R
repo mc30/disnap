58072c2bd5af5564b00cc110265b3682355e39e2
Mikhail Churakov <17121916+mc30@users.noreply.github.com>
2019-10-29 14:38:03 +0100

    Added function to plot strain distribution

--- R/helper.R
+++ R/helper.R
@@ -51,3 +51,55 @@ plotMap <- function(coords, statusMat, column = 1, bLegend = FALSE) {
   if (bLegend)
     legend("topright", legend = types, pch = 19, col = types + 1)
 }
+
+
+
+
+
+
+#' @title Plot strain distribution
+#' @description Plots a barplot with strain distributions for considered timepoints.
+#'
+#' @param statusMat A matrix with statuses (NA, 0, any positive integer) of each unit. Rownames indicate location names, colnames indicate names of timepoints (e.g. years).
+#' @param colors Colors to be used.
+#' @param exclude Strains that should be excluded.
+#' @param ... Additional graphic parameters.
+#' 
+# @return .
+# @details .
+#'
+#' @author Mikhail Churakov
+#'
+#' @export
+plotStrainDistribution <- function(statusMat, colors = 1:ncol(statusMat), exclude = c(), ...) {
+  statusMat[statusMat %in% exclude] <- NA # Exclude particular STs
+  
+  # Merge tables for each timepoint
+  for (i in 1:ncol(statusMat)) {
+    tab <- table(statusMat[statusMat[, i] != 0, i])
+    
+    if (i == 1)
+      temp <- tab
+    else
+      temp <- merge(temp, tab, by = 'Var1', all = TRUE)
+  }
+  
+  temp[is.na(temp)] <- 0 # Replace NAs with zeros
+  mat <- as.matrix(temp)
+  mat <- mat[, -1] # Remove first column (STs)
+  mat <- apply(mat, 1, as.numeric) # Make STs numeric
+  rownames(mat) <- as.character(colnames(statusMat))
+  colnames(mat) <- as.numeric(as.character(temp$Var1)) # STs as rownames
+  mat <- mat[, order(as.numeric(colnames(mat)))] # Sort by STs
+  
+  barplot(mat, beside = TRUE, axisnames = FALSE, col = colors, ...)
+  
+  axis(1, at = (ncol(statusMat) + 1) * c(1:length(colnames(mat))) - ncol(statusMat) / 2,
+       labels = colnames(mat), 
+       cex.axis = 0.8,
+       col.axis = "black",
+       las = 2
+  )
+  
+  legend("topright", border = "true", rownames(mat), cex = 1.0, col = colors, pch = 15, bty = 2)
+}
